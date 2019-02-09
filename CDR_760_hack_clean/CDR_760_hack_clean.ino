d601b4402cef6d5cfbaba2ee55538cbd68a6b3c2
Samwell61 <samuel@bingbong.epac.to>
2019-02-09 23:22:36 +0000

    Added comments

--- CDR_760_hack_clean/CDR_760_hack_clean.ino
+++ CDR_760_hack_clean/CDR_760_hack_clean.ino
@@ -1,6 +1,6 @@
 #include <SPI.h>
 
-volatile bool outputRequired = false;
+volatile bool outputRequired = false; // Flag set in the interrupt
 bool outputTrigger = false;
 long timerStart = 0;
 
@@ -8,29 +8,28 @@ void setup() {
   // turn on SPI in slave mode
   SPCR = (1<<SPIE)|(1<<SPE)|(0<<DORD)|(0<<MSTR)|(1<<CPOL)|(0<<CPHA)|(0<<SPR1)|(0<<SPR0);
 
-
  pinMode(9, OUTPUT); // Enable the override pin
  
 }
 // SPI interrupt routine
 ISR (SPI_STC_vect)
 {
-  if (SPDR == 0x0A){ // If this is the block of 4 bytes that we are interested in
-    outputRequired = true;  
+  if (SPDR == 0x0A){ // If this is the block of 4 bytes that we are interested in...
+    outputRequired = true; // ...need to overwrite the third byte
   }
 }
 
   // end of interrupt service routine (ISR) SPI_STC_vect
 void loop() {
- if (outputRequired){
-    timerStart = micros();
+ if (outputRequired){ // If the block of data we are interested in has been found...
+    timerStart = micros(); // ...start the timer to countown to when we need to output a signal
     outputTrigger = true;
-    outputRequired = false;
+    outputRequired = false; // reset flag
  }
- if(outputTrigger && micros() - timerStart >= 24){
-    PORTB |= _BV(PB1);
-    delayMicroseconds(4);
-    PORTB &= ~_BV(PB1);
-    outputTrigger = false;
+ if(outputTrigger && micros() - timerStart >= 24){ // When we need to output the signal...
+    PORTB |= _BV(PB1); // output the signal
+    delayMicroseconds(4); 
+    PORTB &= ~_BV(PB1); // turn off signal
+    outputTrigger = false; // reset flag
  }
 }
